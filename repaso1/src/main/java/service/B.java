fac3227058dc528eb225676e662f7f7b94b378dd
JJupan <danggamertroll@gmail.com>
2025-08-16 19:46:54 -0600

    Update B.java

--- repaso1/src/main/java/service/B.java
+++ repaso1/src/main/java/service/B.java
@@ -1,11 +1,10 @@
 package service;
-//clase, nombre de clase
+
 public class B {
-    //lo que este dentro de la clase es "atributo
-    //constructor
+   
     public B() {
     }
-    //metodo es toString
+    
 
     @Override
     public String toString() {
