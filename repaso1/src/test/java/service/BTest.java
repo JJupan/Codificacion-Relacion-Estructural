ae29b504a41c9eb827b6c56f34fefdf99b347f5a
JJupan <danggamertroll@gmail.com>
2025-08-16 19:45:13 -0600

    Add files via upload

---
+++ repaso1/src/test/java/service/BTest.java
@@ -0,0 +1,23 @@
+package service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BTest {
+    private B b;
+
+    @Before
+    public void setUp() throws Exception {
+        b = new B();
+
+    }
+    public class BTest {
+    @Test
+    public void testx(){
+        String esperado ="b{}";
+        String obtenido = b,toString() ,toLowerCase();
+        assertEquals( esperado, obtenido);
+    }
+}
